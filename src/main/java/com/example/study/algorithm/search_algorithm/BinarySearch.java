95bb392d9a83bf7a6b9603915b97172ef0443edc
kanghyunwook <kanghw80@gmail.com>
2019-01-19 20:00:54 +0900

    binary recursive

--- src/main/java/com/example/study/algorithm/search_algorithm/BinarySearch.java
+++ src/main/java/com/example/study/algorithm/search_algorithm/BinarySearch.java
@@ -17,29 +17,63 @@ public class BinarySearch {
             .limit(100)
             .collect(Collectors.toList());
 
-        assertThat(binarySearch(integers.toArray(new Integer[integers.size()]), 10)).isEqualTo(9);
+        //assertThat(binarySearch(integers.toArray(new Integer[integers.size()]), 10)).isEqualTo(9);
 
-        assertThat(binarySearch(new Integer[]{1, 3, 5, 7, 9}, 3)).isEqualTo(1);
+        assertThat(binarySearch(new int[]{1, 3, 5, 7, 9}, 3)).isEqualTo(1);
     }
 
-    public int binarySearch(Integer[] list, Integer searchItem) {
+    public int binarySearch(int[] list, int number) {
 
-        int low = 0;
-        int high = list.length - 1;
+        int find = 0;
+        int start = 0, end = list.length - 1;
 
-        while (low <= high) {
+        while (start <= end) {
 
-            int mid = (high + low) / 2;
+            int mid = (start + end) / 2;
 
-            if (list[mid] == searchItem) {
-                return mid;
-            } else if (list[mid] > searchItem) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+            if (list[mid] == number) {
+                find = mid;
+                break;
+            }
+
+            if (list[mid] > number) {
+                //search start to mid-1
+                end = mid - 1;
+
+            } else if (list[mid] < number) {
+                //search mid to end
+                start = mid + 1;
             }
         }
 
-        return 0;
+        return find;
+    }
+
+    @Test
+    public void recursive_test() {
+        assertThat(binarySearchR(new int[]{1, 3, 5, 7, 9}, 3)).isEqualTo(1);
+        assertThat(binarySearchR(new int[]{1, 3, 5, 7, 9}, 8)).isEqualTo(-1);
+    }
+
+    public int binarySearchR(int[] list, int number) {
+
+        return binarySearchR(list, 0, list.length - 1, number);
+    }
+
+    private int binarySearchR(int[] list, int start, int end, int number) {
+
+        if (start > end) {
+            return -1;
+        }
+
+        int mid = (start + end) / 2;
+
+        if (list[mid] < number) {
+            return binarySearchR(list, mid + 1, end, number);
+        } else if (list[mid] > number) {
+            return binarySearchR(list, start, mid - 1, number);
+        } else {
+            return mid;
+        }
     }
 }
