25585398658f62b6600e921c28d3b60e1f433c63
한주희 <lovia98@wemakeprice.com>
2019-02-12 17:47:43 +0900

    merge

---
+++ src/main/java/com/example/study/singleton/EagerInitializedSingleton.java
@@ -0,0 +1,14 @@
+package com.example.study.singleton;
+
+public class EagerInitializedSingleton {
+
+    private static final EagerInitializedSingleton instance = new EagerInitializedSingleton();
+
+    private EagerInitializedSingleton() {
+    }
+
+    public static EagerInitializedSingleton getInstance() {
+        return instance;
+    }
+
+}
